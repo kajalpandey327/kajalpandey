2a383b66b450a39980611270ed7a3df35c92e0f1
kajalpandey327 <pandeykajal550@gmail.com>
2025-07-13 17:52:30 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,11 @@
+const observer = new IntersectionObserver(entries => {
+  entries.forEach(entry => {
+    if (entry.isIntersecting) {
+      entry.target.classList.add('active');
+    }
+  });
+}, {
+  threshold: 0.1
+});
+
+document.querySelectorAll('.scroll-animate').forEach(el => observer.observe(el));
